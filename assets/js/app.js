540f2aacb85348ac66b2a1d1ee94c7ef44719e34
Pritschella <prits99@hotmail.com>
2023-10-04 11:37:41 -0600

    Se agregan las modificaciones de código, así como la funcionalidad del mismo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,30 @@
+/* Descripción del funcionamiento del código:
+
+Código que almacena y muestra la información, utilizando json, clases, variables, funciones e instancias. 
+*/
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// const $n = document.querySelector('name'); línea sin modificar
+const $n = document.querySelector('.name'); //Línea modificada con la .clase
+// const $b = document.querySelector('#blog'); línea sin modificar
+const $b = document.querySelector('.blog'); //Línea modificada con el .blog
+const $l = document.querySelector('.location'); //Línea de código faltante.
 
-function displayUser(username) {
+async function displayUser(username) { //Se agregó la palabra async porque era necesaria
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); //Línea de código que se agregó para el llenado de la tabla
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  /*Líenas de código sin modificar
+   $n.textContent = '${data.name}';
+   $b.textContent = '${data.blog}';
+   $l.textContent = '${data.location}';*/
+
+  //Líneas de código modificadas para darle funcionalidad.
+  $n.textContent = data.name;
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
 }
 
 function handleError(err) {
@@ -19,4 +33,6 @@ function handleError(err) {
   n.textContent = `Algo salió mal: ${err}`
 }
 
+//Línea de código que manda a llamar al método con el usuario
 displayUser('stolinski').catch(handleError);
+
